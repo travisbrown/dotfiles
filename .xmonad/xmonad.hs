592d1fc610e21209adba30785286e0b78b7971ca
Travis Brown <travisrobertbrown@gmail.com>
2012-05-07 09:20:10 -0400

    Cleaner version that spawns xmobar without a pipe.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,39 +1,29 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+  ( avoidStruts
+  , manageDocks
+  , ToggleStruts(ToggleStruts)
+  )
 import XMonad.Hooks.SetWMName (setWMName)
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Util.EZConfig (additionalKeysP)
-import Control.Monad (liftM2)
-import qualified Data.Map as M
 
-toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
-
-myStatusBar conf = do
-  spawn "xmobar"
-  return $ conf
-    { layoutHook = avoidStruts (layoutHook conf)
-    , logHook    = logHook conf
-    , manageHook = manageHook conf <+> manageDocks
-    , keys       = liftM2 M.union keys' (keys conf)
-    }
-    where keys' = (`M.singleton` sendMessage ToggleStruts) . toggleStrutsKey
-
-main = xmonad =<< myStatusBar myConfig
-
-myConfig = defaultConfig
+main = xmonad $ defaultConfig
   { terminal           = "xterm"
   , normalBorderColor  = "#cccccc"
   , focusedBorderColor = "#ff2b06"
-  , layoutHook  = smartBorders $ tiled ||| Mirror tiled ||| Full
+  , layoutHook  = avoidStruts . smartBorders $ tiled ||| Mirror tiled ||| Full
   , manageHook  = myManageHook <+> manageHook defaultConfig
-  , startupHook = setWMName "LG3D" >> broadcastMessage ToggleStruts
+  , startupHook
+    =  setWMName "LG3D"
+    >> spawn "xmobar"
+    >> broadcastMessage ToggleStruts
   }
   `additionalKeysP`
   [ ("M-x c", spawn "chromium")
   , ("M-x f", spawn "firefox")
   , ("M-x t", spawn "thunderbird")
+  , ("M-b",   sendMessage ToggleStruts)
   ]
   where
     tiled   = Tall nmaster delta ratio
